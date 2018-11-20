f6d89b377180d081acf4f92b6078feb1532d7c96
Goran Badevic <goran.badevic@hensoldt-cyber.com>
2018-11-20 17:58:22 +0100

    Add command line utility to send a MQTT publish.

---
+++ mqtt_client/src/SendMqttPublish.cpp
@@ -0,0 +1,45 @@
+
+#include "Socket.h"
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+#define SERVER_PORT 7999
+
+int main(int argc, const char *argv[])
+{
+    int port = SERVER_PORT;
+    string hostName {"127.0.0.1"};
+
+    if (argc > 2)
+    {
+        hostName = string{argv[2]};
+    }
+
+    Socket socket {port, hostName};
+
+    vector<char> publish =
+    {
+       0x32, 0x29, 0x00, 0x21, 0x64, 0x65, 0x76, 0x69, 
+       0x63, 0x65, 0x73, 0x2f, 0x74, 0x65, 0x73, 0x74, 
+       0x5f, 0x64, 0x65, 0x76, 0x2f, 0x6d, 0x65, 0x73, 
+       0x73, 0x61, 0x67, 0x65, 0x73, 0x2f, 0x65, 0x76, 
+       0x65, 0x6e, 0x74, 0x73, 0x2f, 0x00, 0x02, 0x63, 
+       0x69, 0x61, 0x6f
+    };
+
+    //while (true)
+    {
+        int result = socket.Write(publish);
+        if (result < 0)
+        {
+            fprintf(stderr,"Error: write to socket\n");
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+
+    return 0;
+}
