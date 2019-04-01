5125ba847df4f786e166a8e01571d88470080603
Axel Heider <axelheider@gmx.de>
2019-04-01 19:40:47 +0200

    [DEV] improve build script to use subshells and check for directory instead of file

--- build.sh
+++ build.sh
@@ -5,24 +5,26 @@ DIR_SRC=$(dirname $0)
 PICO_SRC=${DIR_SRC}/picotcp
 PICO_BSD=${DIR_SRC}/picotcp-bsd
 
-if [[ ! -e ${PICO_SRC} ]]; then
+if [[ ! -d ${PICO_SRC} ]]; then
   echo "Please add picotcp as a submodule...!"
   exit 1
 else
-  cd ${PICO_SRC}
-  make clean
-  make TAP=1
-  cd ..
+  (
+    cd ${PICO_SRC}
+    make clean
+    make TAP=1
+  )
 fi
 
-if [[ ! -e ${PICO_BSD} ]]; then
+if [[ ! -d ${PICO_BSD} ]]; then
    echo "Please add picotcp-bsd as a submodule...!"
    exit 1
 else
-   cd ${PICO_BSD} 
-   make clean
-   make
-   cd ..
+  (
+    cd ${PICO_BSD}
+    make clean
+    make
+  )
 fi
 
 # build dir will be a subdirectory of the current directory, where this script
