16d4c9a78258912f758607a687fda3731afae54c
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-05-08 14:13:53 +0200

    [FIX] SEOS-174: Increased the buffer size and the max message length.

--- src/Nvm.h
+++ src/Nvm.h
@@ -20,9 +20,9 @@
 #define CMD_WRITE               1
 #define CMD_READ                2
 
-#define MEM_SIZE                1000*4096
+#define MEM_SIZE                1024*1024 //1 MB of memory
 
-#define MAX_MSG_LEN             1024
+#define MAX_MSG_LEN             4096
 #define RESP_HEADER_LEN         6
 
 //INDEXES OF DIFFERENT PARTS OF THE REQUEST MESSAGE (IN A BUFFER)
