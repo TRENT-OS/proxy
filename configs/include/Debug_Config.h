e9b6a58bf7e03bca44a712462fdb38001d675de1
Axel Heider <axel.heider@hensoldt-cyber.com>
2020-03-29 22:49:52 +0200

    [DEV] align code with other modules

--- configs/include/Debug_Config.h
+++
@@ -1,5 +0,0 @@
-#define Debug_Config_STANDARD_ASSERT
-#define Debug_Config_ASSERT_SELF_PTR
-#define Debug_Config_LOG_LEVEL  Debug_LOG_LEVEL_DEBUG
-#define Debug_Config_INCLUDE_LEVEL_IN_MSG
-#define Debug_Config_LOG_WITH_FILE_LINE
