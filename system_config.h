0e60f31afbf401f99db1e4f1644805f17f6d54bb
Axel Heider <axel.heider@hensoldt-cyber.com>
2020-07-23 17:35:50 +0200

    [DEV] set log level to INFO

--- system_config.h
+++ system_config.h
@@ -17,7 +17,7 @@
 #define Debug_Config_INCLUDE_LEVEL_IN_MSG
 #define Debug_Config_LOG_WITH_FILE_LINE
 
-#define Debug_Config_LOG_LEVEL  Debug_LOG_LEVEL_DEBUG
+#define Debug_Config_LOG_LEVEL  Debug_LOG_LEVEL_INFO
 
 
 //-----------------------------------------------------------------------------
